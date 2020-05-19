5c10698ad800b4360264de66bbf096f620a3b540
mrhuy3200 <mrhuy3200113@gmail.com>
2020-05-19 22:15:18 +0700

    all

---
+++ ELearning_V2/DTO/TestQuestionDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ELearning_V2.DTO
+{
+    public class TestQuestionDTO
+    {
+        public long ID { get; set; }
+        public long TestID { get; set; }
+        public long QuestionID { get; set; }
+    }
+}
--- ELearning_V2/Scripts/Pub/TestDetail.js
+++ ELearning_V2/Scripts/Pub/TestDetail.js
@@ -8,6 +8,18 @@ TestDetailApp.controller('TestDetailController', function ($scope, $http, $windo
         LoadTest(TestID);
         LoadQuestion();
         LoadTopic();
+        setTimeout(function () {
+            console.log("Question" + $scope.Questions.length);
+            var temp = $scope.Test.AmountQuestion - $scope.Test.Questions.length;
+            console.log("Test" + temp);
+
+            if ($scope.Questions.length < temp) {
+                $scope.MaxRandom = $scope.Questions.length;
+            }
+            else {
+                $scope.MaxRandom = temp;
+            }
+        }, 5000);
     }
     $scope.lstTopic = [];
     function LoadTest(TestID) {
@@ -31,7 +43,7 @@ TestDetailApp.controller('TestDetailController', function ($scope, $http, $windo
     function LoadQuestion() {
         $http({
             method: 'GET',
-            url: '/Lop/GetListQuesionByUserID'
+            url: '/Lop/GetListQuesionByTestID/' + $scope.TestID
         }).then(function (r) {
             if (r.data == null) {
                 alert("Không có dữ liệu câu hỏi");
@@ -73,17 +85,132 @@ TestDetailApp.controller('TestDetailController', function ($scope, $http, $windo
             }
         });
     }
+    function UpdateQuestionStatus() {
+        for (var i = 0; i < $scope.Questions.length; i++) {
+            var lstTopicID = $scope.Questions[i].Topics;
+            for (var j = 0; j < lstTopicID.length; j++) {
+                $scope.Questions[i]["Hide"] = true;
+                if ($scope.lstTopic.indexOf(lstTopicID[j]) != -1) {
+                    $scope.Questions[i]["Hide"] = false;
+                    break;
+                }
+            }
+        }
+    }
+    function FindQuestion(QuestionID) {
+        for (var i = 0; i < $scope.Questions.length; i++) {
+            if ($scope.Questions[i].ID == QuestionID) {
+                return i;
+            }
+        }
+    }
+    function FindTestQuestion(QuestionID) {
+        for (var i = 0; i < $scope.Test.Questions.length; i++) {
+            if ($scope.Test.Questions[i].ID == QuestionID) {
+                return i;
+            }
+        }
+    }
     $scope.CheckTopic = function (Index) {
         var TopicID = $("#check" + Index).val();
-        console.log($scope.lstTopic)
+        console.log($scope.lstTopic);
         if ($scope.lstTopic.indexOf(TopicID) == -1) {
             $scope.lstTopic.push(TopicID);
+            UpdateQuestionStatus();
         }
         else {
-            $scope.lstTopic.splice($scope.lstTopic.indexOf(TopicID),1)
+            $scope.lstTopic.splice($scope.lstTopic.indexOf(TopicID), 1);
+            UpdateQuestionStatus();
         }
-        console.log($scope.lstTopic)
-
+        console.log($scope.lstTopic);
     }
+    $scope.AddQuestionToTest = function (QuestionID) {
+        if ($scope.Test.Questions.length < $scope.Test.AmountQuestion) {
+            var TestQuestionDTO = {
+                TestID: $scope.TestID,
+                QuestionID: QuestionID
+            };
+            $http({
+                method: 'POST',
+                url: '/Lop/AddQuestionToTest',
+                data: JSON.stringify(TestQuestionDTO)
+            }).then(function (r) {
+                if (r.data == 1) {
+                    $scope.Test.Questions.push($scope.Questions[FindQuestion(QuestionID)]);
+                    $scope.Questions.splice(FindQuestion(QuestionID), 1)
+                    //$scope.Questions[FindQuestion(QuestionID)]["Hide"] = true;
+                }
+                if (r.data == 0) {
+                    alert("Không đủ quyền hạn");
+                }
+                if (r.data == -2) {
+                    alert("Xảy ra lỗi");
+                }
+                if (r.data == -1) {
+                    alert("Câu hỏi đã được thêm trước đó");
+                }
+            });
+        }
+    }
+    $scope.RemoveQuestionFromTest = function (QuestionID) {
+        var TestQuestionDTO = {
+            TestID: $scope.TestID,
+            QuestionID: QuestionID
+        };
+        $http({
+            method: 'POST',
+            url: '/Lop/RemoveQuestionFromTest',
+            data: JSON.stringify(TestQuestionDTO)
+        }).then(function (r) {
+            if (r.data == 1) {
+                $scope.Questions.push($scope.Test.Questions[FindTestQuestion(QuestionID)]);
+                $scope.Test.Questions.splice(FindTestQuestion(QuestionID), 1);
+            }
+            if (r.data == 0) {
+                alert("Không đủ quyền hạn");
+            }
+            if (r.data == -2) {
+                alert("Xảy ra lỗi");
+            }
+            if (r.data == -1) {
+                alert("Câu hỏi chưa dược thêm trước đó");
+            }
+        });
+    }
+    $scope.RandomTestQuestion = function () {
+        var CauHoiDeThi = {
+            ChuongID: $scope.TopicRandom,
+            DoKho: $scope.LevelRandom,
+            SoCauHoi: $scope.AmountRandom,
+            DeThiID: $scope.TestID
+        }
+        $http({
+            method: 'POST',
+            url: '/Lop/RandomTestQuestion',
+            data: JSON.stringify(CauHoiDeThi)
+        }).then(function (r) {
+            if (r.data != null) {
+                console.log(r.data);
+                for (var i = 0; i < r.data.length; i++) {
+                    r.data[i]["FContent"] = $sce.trustAsHtml(r.data[i]["Content"]);
+                    $scope.Test.Questions.push(r.data[i]);
+                    $scope.Questions.splice(FindQuestion(r.data[i].ID), 1)
+                    setTimeout(function () {
+                        MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+                    }, 200);
+                }
+                $scope.TopicRandom = '';
+                $scope.LevelRandom = '';
+                $scope.AmountRandom = '';
+            }
+            if (r.data == 0) {
+                alert("Không đủ quyền hạn");
+            }
+            if (r.data == null) {
+                alert("Xảy ra lỗi");
+            }
+        });
+    }
+
 
 });
